f7ee683a41cfe0ac06e647edd338b9b84b2951aa
Jarrett Bariel <jarrett.bariel@galvanize.com>
2020-05-13 23:38:53 +0000

    empty project dockerized by vscode

---
+++ src/main/java/com/galvanize/springbootgradledockerized/SpringBootGradleDockerizedApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.springbootgradledockerized;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGradleDockerizedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGradleDockerizedApplication.class, args);
+	}
+
+}
